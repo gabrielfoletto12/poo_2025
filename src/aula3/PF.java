83c5469495029d0af5247d75e1c440c6a5b44f01
Gabriel Foletto Lorenzi <gabrielfoletto@dinfor.uniceub>
2025-08-18 20:13:11 -0300

    Primeiro commit do projeto

---
+++ src/aula3/PF.java
@@ -0,0 +1,20 @@
+package aula3;
+
+public class PF extends Pessoa {
+
+	private String cpf;
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	private String dataNascimento;
+	
+}
